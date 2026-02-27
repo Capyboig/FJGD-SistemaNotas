625ad3807b85124b81616776f26bc13d0f35ed7a
Fran <fran@gmail.com>
2026-02-28 00:12:56 +0100

    [27/02/2026 23:12] Guardando inicio de creacion de notas

---
+++ src/repository/NotasRepository.java
@@ -0,0 +1,37 @@
+package repository;
+
+
+import java.io.IOException;
+import java.nio.file.*;
+
+
+public class NotasRepository {
+
+
+    private static final String RUTA_BASE = "data/usuarios";
+
+
+    public static void guardarNota(String emailsanitizado, String titulo, String contenido) {
+        Path carpetaUsuario = Paths.get(RUTA_BASE + emailsanitizado);
+        Path archivoNotas = carpetaUsuario.resolve("notas.txt");
+
+
+
+        try {
+            if (Files.notExists(carpetaUsuario)) {
+                Files.createDirectories(carpetaUsuario);
+            }
+
+            String lineaNota = titulo + ";" + contenido + System.lineSeparator();
+
+
+            Files.writeString(archivoNotas, lineaNota,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+
+            System.out.println("[EXITO] Nota guardada correctamente");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
